e16bc915574dc53fa3399c867b3084284e497d12
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-03-29 19:33:42 +0700

    Add files via upload

---
+++ twrp_X6515.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from X6515 device
+$(call inherit-product, device/infinix/X6515/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X6515
+PRODUCT_NAME := twrp_X6515
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := X6515
+PRODUCT_MANUFACTURER := infinix
